dfb3b29ae5f7b8aa407f07256100c91c9026570a
coding-guru.com <30568161+coding-guruz@users.noreply.github.com>
2022-05-05 04:20:43 +0300

    Minor modification to improve the code

--- CentroidIndex.py
+++ CentroidIndex.py
@@ -7,7 +7,7 @@ Created on Sun Jan 24 21:24:04 2021
 
 import numpy as np
 
-def jaccard_seq(x, y):
+def jaccard(x, y):
     len_x = len(x)
     len_y = len(y)
     fst, snd = (x, y) if len_x < len_y else (y, x)
@@ -16,24 +16,15 @@ def jaccard_seq(x, y):
     #Jaccard score    
     return (num_intersect / (len_x + len_y - num_intersect))
 
-def compute_jaccard_similarity_score(x, y):
-    """
-    Jaccard Similarity J (A,B) = | Intersection (A,B) | /
-                                    | Union (A,B) |
-    """
-    intersection_cardinality = len(set(x).intersection(set(y)))
-    union_cardinality = len(set(x).union(set(y)))
-    return intersection_cardinality / float(union_cardinality)
-
 def compare(labels1, labels2, NUMBER_OF_CLUSTERS):
     results = np.zeros((NUMBER_OF_CLUSTERS,NUMBER_OF_CLUSTERS))
     for n1 in range(0, NUMBER_OF_CLUSTERS):
         indexes1 = np.where(labels1 == n1+1)
         for n2 in range(0, NUMBER_OF_CLUSTERS):
             indexes2 = np.where(labels2 == n2+1)
-            score = jaccard_seq(indexes1[0], indexes2[0])
+            score = jaccard(indexes1[0], indexes2[0])
             results[n1][n2] = int(score*100)
-            #print ("Cluster "+str(n1)+" and cluster "+str(n2)+ ": "+ str(score))
+            
     return results
 
 
@@ -76,6 +67,3 @@ def CentroidIndex(LABELS1,GT):
     CI = max(orphanA, orphanB)
     
     return CI
-
-
-
